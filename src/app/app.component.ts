d769a2494c4008e48c9de0dcaf4008f000ae0be2
sharath.hg <sharath.hg@teams.unvired.com>
2022-05-30 18:00:09 +0530

    changes done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { News, NewsData } from './models/news.model';
-import { NewsService } from './services/news.service';
 
 @Component({
   selector: 'app-root',
