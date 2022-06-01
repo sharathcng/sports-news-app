d7ff68f0fce578eb82c6a6e7dc82bac5a65e1681
sharath.hg <sharath.hg@teams.unvired.com>
2022-06-01 15:50:37 +0530

    updated env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,38 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
   production: true,
-  CricketMatchesBaseUrl: 'https://cricbuzz-cricket.p.rapidapi.com/matches/v1',
+  newsApiBaseUrl: 'https://current-news.p.rapidapi.com/news/',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'current-news.p.rapidapi.com',
+  XRapidAPIHostKeyName: 'X-RapidAPI-Key',
+  XRapidAPIHostKeyValue: 'ad6336067emsh3c3d5e800fd6ae8p148314jsn310f9088fd31',
+
+
+
+  CricketMatchesBaseUrl: 'https://cricbuzz-cricket.p.rapidapi.com/matches/v1/',
   CricketNewsBaseUrl: 'https://cricbuzz-cricket.p.rapidapi.com/news/v1/',
   CricketXRapidAPIHostHeaderName: 'X-RapidAPI-Host',
   CricketXRapidAPIHostHeaderValue: 'cricbuzz-cricket.p.rapidapi.com',
   CricketXRapidAPIHostKeyName: 'X-RapidAPI-Key',
-  CricketXRapidAPIHostKeyValue: 'a36aafa4dbmsh295d5d8a047e859p12cf22jsnef463b0f0965',
-  UpcomingBaseUrl: 'https://cricbuzz-cricket.p.rapidapi.com/schedule/v1'
+  CricketXRapidAPIHostKeyValue: 'ad6336067emsh3c3d5e800fd6ae8p148314jsn310f9088fd31',
+  //CricketXRapidAPIHostKeyValue: 'a36aafa4dbmsh295d5d8a047e859p12cf22jsnef463b0f0965',
+  //CricketXRapidAPIHostKeyValue: '7a352b4145msh22881c4ea81ce53p1b9013jsn0c3de064b2b2',
+
+
+  UpcomingBaseUrl: 'https://cricbuzz-cricket.p.rapidapi.com/schedule/v1/',
+
+  newsDetailBaseUrl: 'https://cricbuzz-cricket.p.rapidapi.com/news/v1/detail/'
+
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
